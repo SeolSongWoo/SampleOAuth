86c9179856326f20103e492488b86adf7a46463a
te2ry <pl9429@naver.com>
2022-12-08 13:42:29 +0900

    환경설정

---
+++ src/main/java/com/example/webstudyproject/WebStudyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.webstudyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStudyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStudyProjectApplication.class, args);
+    }
+
+}
